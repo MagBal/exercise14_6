43ec34de329a3fdfddd903e238deeae53603d1da
Magda Balun <magi@op.pl>
2018-01-10 10:41:17 +0100

    Create files

--- script.js
+++ script.js
@@ -68,6 +68,8 @@ var Counter = React.createClass({
 var element =
     React.createElement('div', {},
         React.createElement(Counter, {}),
+        React.createElement(Counter, {}),
+        React.createElement(Counter, {})
     );
 
 ReactDOM.render(element, document.getElementById('app'));
